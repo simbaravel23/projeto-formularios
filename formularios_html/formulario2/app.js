01f8b81c5b039280125e031350a62c6d61a997d2
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-09-09 20:32:43 -0300

    forms

---
+++ formularios_html/formulario2/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Configuração do Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Servir arquivos estáticos (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuração do Banco de Dados MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'simba123',
+    database: 'meubanco1'
+});
+
+db.connect(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Conectado ao banco de dados MySQL');
+});
+
+// Rota para exibir o formulário HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rota para processar o formulário
+app.post('/submit', (req, res) => {
+    const { animal, raca } = req.body;
+    const query = 'INSERT INTO usuarios (animal, raca) VALUES (?, ?)';
+    db.query(query, [animal, raca], (err, results) => {
+        if (err) {
+            res.status(500).send('Erro ao inserir dados');
+            throw err;
+        }
+        res.send('Dados inseridos com sucesso');
+    });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
